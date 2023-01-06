fcf3b1bb2efc51f5de0e68788a56904bfb1894e4
Yan <yanpangee@gmail.com>
2023-01-06 23:53:25 +0800

    Update index.js
    Add the topic of MDPI

--- src/pages/index.js
+++ src/pages/index.js
@@ -134,7 +134,7 @@ const IndexPage = () => {
             <div className="text-blue-800 text-xl font-bold">News</div>
             <div className="text-sm pl-10 py-3">
               <ul className="list-disc">
-                <li>[Jan 06, 2022] <b>NEW:</b> Become a topic editor of the "<a className="text-blue-600 text-sm" href="https://www.mdpi.com/topics/FLN51J9SH1" target="_blank">AI and Data-Driven Advancements in Industry 4.0</a>". Welcome to submit your papers on AI.</li>
+                <li>[Jan 06, 2023] <b>NEW:</b> Become a topic editor of the "<a className="text-blue-600 text-sm" href="https://www.mdpi.com/topics/FLN51J9SH1" target="_blank">AI and Data-Driven Advancements in Industry 4.0</a>". Welcome to submit your papers on AI.</li>
                 <li>[Aug 15, 2022] Joined to Guangzhou University, Guangdong.</li>
                 <li>[Jul 25, 2022] Two papers published to Journal of International Journal of Intelligent Systems.</li>
                 <li>[Jun 20, 2022] Submitted one paper to Journal of IEEE Transactions on Neural Networks and Learning System.</li>
@@ -228,8 +228,8 @@ const IndexPage = () => {
         
 
         <div className="h-48 bg-gray-200 text-center pt-16 text-sm text-gray-500">
-          <div>© 2022 Yan Pang. All rights reserved</div>
-          <div>(Last update: Sep 19, 2022.)</div>
+          <div>© 2023 Yan Pang. All rights reserved</div>
+          <div>(Last update: Jan 06, 2023.)</div>
         </div>
       </div>
     </main>
