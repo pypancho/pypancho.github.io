f2e74fab2e0819bd225e6ce66cc065b0f6cc891f
Yan <yanpangee@gmail.com>
2023-01-06 23:09:36 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -135,7 +135,6 @@ const IndexPage = () => {
             <div className="text-sm pl-10 py-3">
               <ul className="list-disc">
                 <li>[Jan 07, 2022] <b>NEW:</b> Become the topic editor of "<a className="text-blue-600 text-sm" href="https://www.mdpi.com/topics/FLN51J9SH1" target="_blank">AI and Data-Driven Advancements in Industry 4.0</a>"</li>
-                <li>[Jan 07, 2022] <b>NEW:</b> Become the topic editor of "AI and Data-Driven Advancements in Industry 4.0".</li>
                 <li>[Aug 15, 2022] Joined to Guangzhou University, Guangdong.</li>
                 <li>[Jul 25, 2022] Two papers published to Journal of International Journal of Intelligent Systems.</li>
                 <li>[Jun 20, 2022] Submitted one paper to Journal of IEEE Transactions on Neural Networks and Learning System.</li>
