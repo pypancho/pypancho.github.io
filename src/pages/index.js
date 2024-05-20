18e467ba9576f0e46367aad49adc1393b08f2aa5
Yan <yanpangee@gmail.com>
2024-05-20 14:39:04 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -237,7 +237,7 @@ const IndexPage = () => {
 
 
                 </li>
-                <li>Weiqing Kong, Ziyu Ding, Yile Hong, Hui Li, Mingwei Chen</li>
+                <li>Jie Hong, Yile Hong, Hui Li, Mingwei Chen</li>
                 <li>Yanjun Ming, Mingdu Zhang</li>
                 </ul>
             </div>
@@ -248,7 +248,7 @@ const IndexPage = () => {
         
 
         <div className="h-48 bg-gray-200 text-center pt-16 text-sm text-gray-500">
-          <div>© 2023 Yan Pang. All rights reserved</div>
+          <div>© 2024 Yan Pang. All rights reserved</div>
           <div>(Last update: Apr 11, 2024.)</div>
         </div>
       </div>
