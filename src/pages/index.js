131db6cbc59d5c11e223c68f0485ca4fd5d4d0e6
Yan <yanpangee@gmail.com>
2025-06-27 09:32:54 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -310,8 +310,8 @@ const IndexPage = () => {
             <div className="text-sm pl-10 py-3">
               <ul className="list-disc">
                 <li>2024: Jie Hong, Dan Li, <a className="text-blue-600 text-sm" href={"https://jeming-creater.github.io/"} target="_blank">Jiaming Liang</a></li>
-                <li>2025: Yile Hong, Hui Li, Mingwei Chen</li>
-                <li>2026: <a className="text-blue-600 text-sm" href={"https://peterli.club/"} target="_blank">Yunhao Li</a>, Caiyan Tan, Yanjun Ming, Mingdu Zhang, Junming Zi, Xiangfu Liu</li>
+                <li>2025: <a className="text-blue-600 text-sm" href={"https://peterli.club/"} target="_blank">Yunhao Li</a>, Yile Hong, Hui Li, Mingwei Chen</li>
+                <li>2026: Caiyan Tan, Yanjun Ming, Mingdu Zhang, Junming Zi, Xiangfu Liu</li>
                 </ul>
             </div>
         </div>
