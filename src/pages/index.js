85f9378b24f200c2a550c3443e94741a32334c3f
Yan <yanpangee@gmail.com>
2025-08-23 11:55:43 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -180,26 +180,27 @@ const IndexPage = () => {
             <div className="text-blue-800 text-xl font-bold">News</div>
             <div className="text-sm pl-10 py-3">
               <ul className="list-disc">
-                <li>[Jul 30, 2025] <b>NEW:</b> One paper is published in the IEEE Transactions on Big Data. (IF: 5.7).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/Consense" target="_blank">Code</a> is released.</li>
-                <li>[Jul 14, 2025] <b>NEW:</b> One paper is published in the IEEE Transactions on Information Forensics and Security. (IF: 8).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/SAMamba" target="_blank">Code</a> is released.</li>
-                <li>[Jun 17, 2025] One paper is published in the IEEE Transactions on Systems, Man and Cybernetics: Systems. (IF: 8.7).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/LGA" target="_blank">Code</a> is released.</li>
-                <li>[Feb 19, 2025] One paper is published in the IEEE Journal of Biomedical and Health Informatics. (IF: 7.7).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/BaS" target="_blank">Code</a> is released.</li>
-                <li>[Jan 16, 2025] One paper is published in the IEEE Journal of Biomedical and Health Informatics. (IF: 7.7).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/MOD" target="_blank">Code</a> is released.</li>
+                <li>[Oct 23, 2024] <b>NEW:</b> 🎉 Success! Papers 1614, 1833, and 2000 from our group are presented at <a className="text-blue-600 text-sm" href="http://2025.prcv.cn/" target="_blank">PRCV 2025</a> in Shanghai. Proud of my students' contributions to the field! </li>
+                <li>[Jul 30, 2025] <b>NEW:</b> One paper has been published in the IEEE Transactions on Big Data. (IF: 5.7).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/Consense" target="_blank">Code</a> is released.</li>
+                <li>[Jul 14, 2025] One paper has been published in the IEEE Transactions on Information Forensics and Security. (IF: 8).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/SAMamba" target="_blank">Code</a> is released.</li>
+                <li>[Jun 17, 2025] One paper has been published in the IEEE Transactions on Systems, Man and Cybernetics: Systems. (IF: 8.7).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/LGA" target="_blank">Code</a> is released.</li>
+                <li>[Feb 19, 2025] One paper has been published in the IEEE Journal of Biomedical and Health Informatics. (IF: 7.7).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/BaS" target="_blank">Code</a> is released.</li>
+                <li>[Jan 16, 2025] One paper has been published in the IEEE Journal of Biomedical and Health Informatics. (IF: 7.7).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/MOD" target="_blank">Code</a> is released.</li>
                 <li>[Dec 12, 2024] <b>NEW:</b> Welcome to submit your papers to our special topics "<a className="text-blue-600 text-sm" href="https://www.mdpi.com/topics/0ZZS608U21" target="_blank">AI and Data-Driven Advancements in Industry 4.0, 2nd Edition</a>".</li>
                 <li>[Oct 19, 2024] Honored to be named one of the best ACs for PRCV2024! Excited to contribute to this leading conference in computer vision.</li>
-                <li>[Oct 18, 2024] 🎉 Success! Papers 1458, 1464, and 1468 from our group are presented at <a className="text-blue-600 text-sm" href="http://www.prcv.cn/" target="_blank">PRCV 2024</a> in Urumqi. Proud of my students' contributions to the field! </li>
-                <li>[Apr 21, 2024] One paper is published in the Journal of IEEE Transactions on Instrumentation and Measurement. (IF: 5.6).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/AdaptFormer" target="_blank">Code</a> is released.</li>
-                <li>[Oct 20, 2023] One paper is published in the Journal of IEEE Transactions on Medical Imaging. (IF: 10.6).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/Slim-UNETR" target="_blank">Code</a> is released.</li>
-                <li>[Aug 14, 2023] One paper is published in the Journal of IEEE Transactions on Neural Networks and Learning Systems. (IF: 10.4).</li>
+                <li>[Oct 18, 2024] 🎉 Success! Papers 1458, 1464, and 1468 from our group are presented at <a className="text-blue-600 text-sm" href="http://2024.prcv.cn/" target="_blank">PRCV 2024</a> in Urumqi. Proud of my students' contributions to the field! </li>
+                <li>[Apr 21, 2024] One paper has been published in the Journal of IEEE Transactions on Instrumentation and Measurement. (IF: 5.6).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/AdaptFormer" target="_blank">Code</a> is released.</li>
+                <li>[Oct 20, 2023] One paper has been published in the Journal of IEEE Transactions on Medical Imaging. (IF: 10.6).  <a className="text-blue-600 text-sm" href="https://github.com/deepang-ai/Slim-UNETR" target="_blank">Code</a> is released.</li>
+                <li>[Aug 14, 2023] One paper has been published in the Journal of IEEE Transactions on Neural Networks and Learning Systems. (IF: 10.4).</li>
                 <li>[Feb 07, 2023] One paper published in the Journal of Information Sciences (IF: 8.233).</li>
                 <li>[Jan 06, 2023] Become a topic editor of the "<a className="text-blue-600 text-sm" href="https://www.mdpi.com/topics/FLN51J9SH1" target="_blank">AI and Data-Driven Advancements in Industry 4.0</a>". Welcome to submit your papers on AI.</li>
                 <li>[Aug 15, 2022] Joined to Guangzhou University, Guangdong.</li>
-                <li>[Jul 25, 2022] Two papers published in the Journal of International Journal of Intelligent Systems （IF：8.993）.</li>
+                <li>[Jul 25, 2022] Two papers published in the International Journal of Intelligent Systems （IF：8.993）.</li>
                 <li>[Dec 18, 2021] Received my Ph.D. degree.</li>
                 <li>[Aug 01, 2021] One paper published in the Journal of Geophysical Research Letters (IF: 5.58). </li>
                 <li>[Apr 12, 2021] Joined to Moffett AI, Los Altos, CA.</li>
-                <li>[Nov 01, 2020] One paper is published in the Journal of Computers and Electronics in Agriculture. </li>
-                <li>[Mar 01, 2020] One paper is published in the Journal of Neuroscience Method. </li>
+                <li>[Nov 01, 2020] One paper has been published in the Journal of Computers and Electronics in Agriculture. </li>
+                <li>[Mar 01, 2020] One paper has been published in the Journal of Neuroscience Methods. </li>
                 <li>[Apr 24, 2019] One paper is accepted to Science and Information Conference</li>
               </ul>
             </div>
@@ -354,7 +355,7 @@ const IndexPage = () => {
 
         <div className="h-48 bg-gray-200 text-center pt-16 text-sm text-gray-500">
           <div>© 2025 Yan Pang. All rights reserved</div>
-          <div>(Last update: Jul 30, 2025.)</div>
+          <div>(Last update: Aug 23, 2025.)</div>
         </div>
       </div>
     </main>
