695ae00f10a05c1dd6b40783ea89418ad42d4bb1
Yan <yanpangee@gmail.com>
2025-10-13 22:10:53 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -380,7 +380,7 @@ const IndexPage = () => {
 
         <div className="h-48 bg-gray-200 text-center pt-16 text-sm text-gray-500">
           <div>© 2025 Yan Pang. All rights reserved</div>
-          <div>(Last update: Oct 01, 2025.)</div>
+          <div>(Last update: Oct 10, 2025.)</div>
         </div>
       </div>
     </main>
